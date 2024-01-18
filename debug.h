eeb2732e6646d0d3d6af6373f4eb9c0127f4f8f0
Muhammad Dinislam <108214424+Md-DinIslam@users.noreply.github.com>
2024-01-19 00:39:47 +0600

    debug.h

--- debug.h
+++ debug.h
@@ -25,8 +25,8 @@ void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++
 void _print() {cerr << "]\n";}
 template <typename T, typename... V>
 void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
 
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
 
 // #ifndef LOCAL
 // #define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
